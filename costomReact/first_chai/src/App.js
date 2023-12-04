5688d997e216d0d45e48f6ba1e5e9fd7bd37e507
Random-404error <124092747+Random-404error@users.noreply.github.com>
2023-12-04 15:18:36 +0530

    correcting first_chai..
    ignore blank folder

---
+++ costomReact/first_chai/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Chai from './Chai';
+
+function App() {
+  return (
+   <>
+   <h1>hello...!</h1>
+   <Chai/>
+   </>
+  );
+}
+
+export default App;
---
+++ costomReact/first_chai/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai (){
+    return (
+        <>'
+        <h3>your Chai Is Ready....!</h3>
+        </>
+    )
+}
+
+export default Chai;
